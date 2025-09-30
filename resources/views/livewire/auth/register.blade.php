c7e848f0308ffa475b9a9478f8ac5bd8252ddd2e
gabrieladrianofx <gabrieladrianofx@gmail.com>
2025-09-30 00:30:49 -0300

    crm-1: register form front-end

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,13 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
-</div>
+<x-card shadow class="mx-auto w-[350px]">
+    <x-form wire:submit="submit">
+        <x-input label="Name" wire:model="name" />
+        <x-input label="Email" wire:model="email" />
+        <x-input label="Confirm your email" wire:model="email_confirmation" />
+        <x-input label="password" wire:model="password" type="password" />
+
+        <x-slot:actions>
+            <x-button label="Cancel" type="reset" />
+            <x-button label="Register" class="btn-primary" type="submit" spinner="submit" />
+        </x-slot:actions>
+    </x-form>
+</x-card>
