38d6265c5838415fd99c2319e10efe5faccfbc14
gabrieladrianofx <gabrieladrianofx@gmail.com>
2025-10-01 11:55:26 -0300

    crm-2: ensuring login to be secured by rate limite

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -2,14 +2,15 @@
 
 namespace App\Livewire\Auth;
 
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\{Auth, RateLimiter};
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class Login extends Component
 {
-    public ?string $email = null;
+    public ?string $email;
 
-    public ?string $password = null;
+    public ?string $password;
 
     public function render()
     {
@@ -18,12 +19,27 @@ class Login extends Component
 
     public function tryToLogin()
     {
+        if (RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            $this->addError('rateLimiter', trans('auth.throttle', [
+                'seconds' => RateLimiter::availableIn($this->throttleKey()),
+            ]));
+
+            return;
+        }
+
         if (!Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
+            RateLimiter::hit($this->throttleKey());
+
             $this->addError('invalidCredentials', trans('auth.failed'));
 
             return;
         }
 
         $this->redirect(route('dashboard'));
+    }
+
+    private function throttleKey(): string
+    {
+        return Str::transliterate(Str::lower($this->email) . '|' . request()->ip());
     }
 }
